da3496cb52f9a8211a5abf9c1102665a03b27624
Felipe Sobreira <felipesobreira.c@hotmail.com>
2020-09-08 23:47:08 -0300

    ao montar a aplicação, fazemos um fetch da API, buscando dados de todos os países

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ export default class App extends Component {
     };
   }
 
+  async componentDidMount() {
+    const res = await fetch('https://restcountries.eu/rest/v2/all');
+    const json = await res.json();
+
+    //console.log(json);
+  }
+
   render() {
     return <h1>React Countries</h1>;
   }
